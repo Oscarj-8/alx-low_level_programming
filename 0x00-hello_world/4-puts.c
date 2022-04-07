874a238a776e9b75ed854e1f0d4578ce7b40fcc2
Oscarj-8 <mmmabdulahi954@yahoo.com>
2022-04-07 23:38:57 +0300

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
 * main - write "programming is like building a mulilingual puzzle"
+*
 * return: Always 0 (success)
 */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

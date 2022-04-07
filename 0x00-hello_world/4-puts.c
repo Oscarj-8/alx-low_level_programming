864ab501b4671dccd9a95c41219380f76890c577
Oscarj-8 <mmmabdulahi954@yahoo.com>
2022-04-07 22:34:49 +0300

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/** 
+ * main - write "programming is like building a mulilingual puzzle"
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+  puts("\"programming is like building a multilingual puzzle");
+  return (0);
+} 

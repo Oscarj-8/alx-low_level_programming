95f31aa2a4c256785aa4519a131ab2ad59eb0398
Oscarj-8 <mmmabdulahi954@yahoo.com>
2022-04-08 00:12:07 +0300

    Hello,printf 1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry point
- * my hello,printf task
- * return: return 0 after pg execution
+ *main - entry point
+ *my hello,printf task
+ *return: return 0 after pg execution
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 

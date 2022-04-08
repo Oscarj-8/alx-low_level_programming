e3523632fba96c4836b188c7e1de4fa5439385d0
Oscarj-8 <mmmabdulahi954@yahoo.com>
2022-04-08 13:17:24 +0300

    posetive is better than negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * return - always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n=rand() - RAND_MAX / 2;
+
+if (n==0)
+{
+printf("%i is zero\n", n);
+}
+
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+
+else
+{
+printf("%i is posetive\n", n);
+}
+
+return (0);
+
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Here we come variables,if,else and while

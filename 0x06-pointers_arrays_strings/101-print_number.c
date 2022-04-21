0a86bd28bd75c501ef7ea7712af115106cb60ce1
Oscarj-8 <mmmabdulahi954@yahoo.com>
2022-04-21 12:00:30 +0300

    done

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,15 +12,15 @@ void print_number(int n)
  unsigned int num;
 /*check if number is negative*/
  num = n;
- if (n < 0)
- {
-  _putchar(45);
-  num = -n;
- }
+if (n < 0)
+{
+ _putchar(45);
+ num = -n;
+}
 /* print number by recursion*/
- if (num / 10)
- {
-  print_number(num / 10);
- }
+if (num / 10)
+{
+ print_number(num / 10);
+}
  _putchar((num % 10) + '0');
 }

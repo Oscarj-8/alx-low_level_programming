ccd20949303a937156e8dec89c401c4871148dc0
Oscarj-8 <mmmabdulahi954@yahoo.com>
2022-04-25 20:36:53 +0300

    task 0 done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,13 @@
 char *_memset(char *s, char b, unsigned int n)
 
 {
-	unsigned int 1;
+	unsigned int i;
 
-	/*declaring FOR*/
+	/*Delacring FOR*/
+	for (i = 0; i < n; i++)
 	{
-		*(s + 1) = b;
+		*(s + i) = b; /*add 1 position s*/
+
 	} /*END FOR*/
 
 	return (s);
